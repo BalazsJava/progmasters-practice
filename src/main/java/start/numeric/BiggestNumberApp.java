f319e4099061414b3d16fd8e2b124b40d26fc296
BalazsJava <balazs.java@gmail.com>
2024-01-24 17:01:26 +0100

    PRC-2 Add biggest number application

---
+++ src/main/java/start/numeric/BiggestNumberApp.java
@@ -0,0 +1,29 @@
+package start.numeric;
+
+import java.util.Scanner;
+
+public class BiggestNumberApp {
+
+    /**
+     * Prints out the biggest number from the input.
+     *
+     * @param args the numbers provided by the user.
+     */
+    public static void main(String[] args) {
+        //Implement an application that takes 3 numbers (int) in a row, then it should print the biggest number
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        if (c > b && c > a) {
+            System.out.println(c);
+        } else if (a > b && b < c) {
+            System.out.println(a);
+        } else if (b > a && c < b) {
+            System.out.println(b);
+        } else if (a == b && a == c) {
+            System.out.println("The biggest" );
+        }
+    }
+}
